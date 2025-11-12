8d0a010e8c1f35d4aef48ffa1fba3db9faa6a2b1
Ripaldy <paldy1512@gmail.com>
2025-11-12 12:49:52 +0700

    fix: Simplify Filament stats styling - use CSS selectors on extraAttributes classes instead of custom blade

--- sipekan/app/Filament/Widgets/StatsOverview.php
+++ sipekan/app/Filament/Widgets/StatsOverview.php
@@ -11,8 +11,6 @@ use Illuminate\Support\Facades\DB;
 
 class StatsOverview extends BaseWidget
 {
-    protected string $view = 'filament.widgets.stats-overview';
-
     protected function getStats(): array
     {
         // Total Anak Terdaftar
@@ -74,16 +72,5 @@ class StatsOverview extends BaseWidget
                 ->chartColor('#27ae60')
                 ->extraAttributes(['class' => 'stat-success']),
         ];
-    }
-
-    protected function getStatColor(string $color): string
-    {
-        return match($color) {
-            'info' => '#3498db',
-            'warning' => '#f1c40f',
-            'danger' => '#e74c3c',
-            'success' => '#27ae60',
-            default => '#6b7280',
-        };
     }
 }
